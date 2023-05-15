22400b007e9ab1cc2129a5bf1a3b404d4c847146
MiguelAngelLardo <miguel.lardo@yahoo.com.ar>
2023-05-15 19:48:41 -0300

    Formulario con SPAN rojo

--- js/formulario.js
+++ js/formulario.js
@@ -4,22 +4,23 @@ boton = document.getElementById('b1');
 
 formulario.addEventListener("submit", function(e) {
 var algunError = false;
+var mensajeError = document.getElementById('mensajeError')
 
 if (formulario.nombre.value == '' || formulario.ciudad.value == '' || formulario.telefono.value == '' || formulario.email.value == '' || formulario.mayores.value == '' || formulario.menores.value == '') {
-  alert("Completa TODOS los campos por favor.");
+  mensajeError.textContent = "*Completa TODOS los campos por favor.";
   algunError = true;
 }
 
 if (formulario.ingreso.value == '' || formulario.salida.value == '') {
   if (!algunError) {
-  alert("Completa las fechas de ingreso y salida");
+  mensajeError.textContent = "*Completa las fechas de ingreso y salida";
   algunError = true;
 }
 }
 
 if (formulario.terminos.checked == false) {
   if (!algunError) {
-    alert("Acepta los términos y condiciones");
+    mensajeError.textContent = "*Acepta los términos y condiciones";
     algunError = true;
   }
 }
