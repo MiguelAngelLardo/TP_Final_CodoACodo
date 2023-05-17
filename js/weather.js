638f5aa5b4aca1f8fc3775d61a50f20db90bb1c4
MiguelAngelLardo <miguel.lardo@yahoo.com.ar>
2023-05-17 11:43:49 -0300

    API del  clima en Contacto

---
+++ js/weather.js
@@ -0,0 +1,25 @@
+const obtenerClimaPorCoordenadas = () => {
+  const latitud = -34.6037; // Latitud de Buenos Aires
+  const longitud = -58.3816; // Longitud de Buenos Aires
+
+  fetch(`https://www.meteosource.com/api/v1/free/point?lat=${latitud}&lon=${longitud}&sections=current&timezone=UTC&language=en&units=metric&key=uhvh6bklldr3s2505a4hcyczvzz6dlpx26c988b4`)
+    .then(response => response.json())
+    .then(data => {
+      console.log(data);
+      const climaActual = data.current; // Accede al objeto de clima actual dentro de la respuesta JSON
+
+      // Accede a la temperatura dentro del objeto climaActual
+      const temperatura = climaActual.temperature;
+      
+      // Muestra la temperatura en el elemento de noticias
+      const elementoClima = document.getElementById('elemento-clima');
+      elementoClima.innerHTML = `<p><i class="fas fa-thermometer-three-quarters" style="color: #2f5bac;"></i> Temperatura: ${temperatura} °C</p>`;
+    })
+    .catch(error => {
+      console.log('Error al obtener los datos del clima:', error);
+    });
+};
+
+document.addEventListener('DOMContentLoaded', function() {
+  obtenerClimaPorCoordenadas();
+});
