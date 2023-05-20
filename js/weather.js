11e3a155b14a3e25d2a27a78dae10e179a8bf27a
MiguelAngelLardo <miguel.lardo@yahoo.com.ar>
2023-05-20 15:55:14 -0300

    Correccion identados

--- js/weather.js
+++ js/weather.js
@@ -1,6 +1,6 @@
 const obtenerClimaPorCoordenadas = () => {
-  const latitud = -34.6037; // Latitud de Buenos Aires
-  const longitud = -58.3816; // Longitud de Buenos Aires
+  const latitud = -34.6037; //**-34.6037 Latitud de Buenos Aires** **40.4168 Latitud de Madrid**
+  const longitud = -58.3816; //**-58.3816 Longitud de Buenos Aires** **-3.7038 Longitud de Madrid**
 
   fetch(`https://www.meteosource.com/api/v1/free/point?lat=${latitud}&lon=${longitud}&sections=current&timezone=UTC&language=en&units=metric&key=uhvh6bklldr3s2505a4hcyczvzz6dlpx26c988b4`)
     .then(response => response.json())
@@ -8,10 +8,8 @@ const obtenerClimaPorCoordenadas = () => {
       console.log(data);
       const climaActual = data.current; // Accede al objeto de clima actual dentro de la respuesta JSON
 
-      // Accede a la temperatura dentro del objeto climaActual
       const temperatura = climaActual.temperature;
       
-      // Muestra la temperatura en el elemento de noticias
       const elementoClima = document.getElementById('elemento-clima');
       elementoClima.innerHTML = `<p><i class="fas fa-thermometer-three-quarters" style="color: #2f5bac;"></i> Temperatura: ${temperatura} °C</p>`;
     })
